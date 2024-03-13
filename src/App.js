d6a1b2d4c877081c10c34c2f1ead8f0ab5ea4910
BLEAX <rlawjdgns135@naver.com>
2024-03-13 17:09:29 +0900

    1
    1

--- src/App.js
+++ src/App.js
@@ -1,21 +1,56 @@
+import { Component } from "react"; // 컴포넌트 상속 위해 Component 불러오기
 import "./App.css";
 
-class Subject extends Component { //컴포넌트 함수
-  render() {  //
+class Subject extends Component {
+  render() {
     return (
       <header>
-        <h1>how to use react component</h1>
+        <h1>WEB</h1>
+        World wide web
       </header>
+    );
+  }
+}
+
+class TOC extends Component {
+  render() {
+    return (
+      <nav>
+        <ul>
+          <li>
+            <a href='1.html'>HTML</a>
+          </li>
+          <li>
+            <a href='2.html'>CSS</a>
+          </li>
+          <li>
+            <a href='3.html'>JavaScript</a>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+class Content extends Component {
+  render() {
+    return (
+      <article>
+        <h2>HTML</h2>
+        HTML is HyperText Markup Language
+      </article>
     );
   }
 }
 
 class App extends Component {
   render() {
-    return ( 
-    <div className='App'>
-      <Subject></Subject>
-    </div>
+    return (
+      <div className='App'>
+        <Subject></Subject>
+        <TOC></TOC>
+        <Content></Content>
+      </div>
     );
   }
 }
